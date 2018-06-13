543997143655e2e9fe61abbabfda61c89ec33f59
Valeryia Halasnaya <vgolosnaya@gmail.com>
2018-06-13 22:47:24 +0300

    Merge branch semantic-ui to master

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
     NavLink,
     Redirect,
@@ -12,110 +12,110 @@ import {connect} from 'react-redux';
 import Login from '../scenes/Login/Login';
 import * as actions from './services/actions';
 import * as authActions from '../scenes/Login/services/actions';
-import { UserBar } from './components/UserBar/UserBar';
+import {UserBar} from './components/UserBar/UserBar';
 import FavouritesScene from '../scenes/Favourites/Favourites';
 import LoadingBar, {
-  hideLoading,
-  showLoading
+    hideLoading,
+    showLoading
 } from 'react-redux-loading-bar';
 import $ from 'jquery';
 import PropTypes from 'prop-types';
-import { bindActionCreators } from 'redux';
+import {bindActionCreators} from 'redux';
 
 export class Main extends Component {
 
-  componentDidMount() {
-    this.props.onAppInit();
-    $(document).on('ajaxStart', this.props.actions.showLoading);
-    $(document).on('ajaxStop', this.props.actions.hideLoading);
-  }
-
-  componentWillUnmount() {
-    $(document).off('ajaxStart', this.props.actions.showLoading);
-    $(document).off('ajaxStop', this.props.actions.hideLoading)
-  }
-
-  handleLogout = () => {
-    this.props.onLogout();
-  };
-
-  render() {
-    const {user, loggedIn, initializing, error} = this.props;
-
-    if (error) {
-      return <div>{error}</div>;
+    componentDidMount() {
+        this.props.onAppInit();
+        $(document).on('ajaxStart', this.props.actions.showLoading);
+        $(document).on('ajaxStop', this.props.actions.hideLoading);
     }
 
-    const pageName = this.props.location.pathname.substr(1).replace('/', '-');
+    componentWillUnmount() {
+        $(document).off('ajaxStart', this.props.actions.showLoading);
+        $(document).off('ajaxStop', this.props.actions.hideLoading)
+    }
 
-    return (
-    <React.Fragment>
-        <LoadingBar/>
-        {initializing ? 'Loading!' : null}
-      <div className="ui pointing menu">
+    handleLogout = () => {
+        this.props.onLogout();
+    };
 
-        <div className='app-header__logo'>
-          GIPHY Search | {pageName}
-        </div>
-        <NavLink to={'/random'} className="item">Random</NavLink>
-        <NavLink to={'/home'}  className="item">Home</NavLink>
+    render() {
+        const {user, loggedIn, initializing, error} = this.props;
 
-          {loggedIn ?
-              <NavLink to={'/favourites'}  className="item">Favourites</NavLink> : null
-          }
+        if (error) {
+            return <div>{error}</div>;
+        }
 
-          {!loggedIn && !initializing ?
-              <NavLink to={'/login'}  className="item">Login</NavLink> : null
-          }
-          {loggedIn ?
-              <UserBar user={user} onLogout={this.handleLogout}/> : null
-          }
-      </div>
-      <Switch>
-        <Route exact path='/' component={RandomScene}/>
-        <Route path={'/random'} component={RandomScene}/>
-        <Route path={'/home'} component={HomeScene}/> : null
+        const pageName = this.props.location.pathname.substr(1).replace('/', '-');
 
-          {loggedIn ?
-              <Route path={'/favourites'} component={FavouritesScene}/> : null
-          }
+        return (
+            <React.Fragment>
+                <LoadingBar/>
+                {initializing ? 'Loading!' : null}
+                <div className="ui pointing menu">
 
-          {!loggedIn && !initializing ?
-              <Route path={'/login'} component={Login}/> : null
-          }
+                    <div className='app-header__logo'>
+                        GIPHY Search | {pageName}
+                    </div>
+                    <NavLink to={'/random'} className="item">Random</NavLink>
+                    <NavLink to={'/home'} className="item">Home</NavLink>
 
-        <Redirect to={'/random'}/>
-      </Switch>
-    </React.Fragment>
-    );
-  }
+                    {loggedIn ?
+                        <NavLink to={'/favourites'} className="item">Favourites</NavLink> : null
+                    }
+
+                    {!loggedIn && !initializing ?
+                        <NavLink to={'/login'} className="item">Login</NavLink> : null
+                    }
+                    {loggedIn ?
+                        <UserBar user={user} onLogout={this.handleLogout}/> : null
+                    }
+                </div>
+                <Switch>
+                    <Route exact path='/' component={RandomScene}/>
+                    <Route path={'/random'} component={RandomScene}/>
+                    <Route path={'/home'} component={HomeScene}/> : null
+
+                    {loggedIn ?
+                        <Route path={'/favourites'} component={FavouritesScene}/> : null
+                    }
+
+                    {!loggedIn && !initializing ?
+                        <Route path={'/login'} component={Login}/> : null
+                    }
+
+                    <Redirect to={'/random'}/>
+                </Switch>
+            </React.Fragment>
+        );
+    }
 
 }
 
 const mapStateToProps = (state) => {
-  const {user, loggedIn} = state.auth;
-  const {initializing, error} = state.main;
+    const {user, loggedIn} = state.auth;
+    const {initializing, error} = state.main;
 
-  return {
-    user,
-    loggedIn,
-    initializing,
-    error
-  };
+    return {
+        user,
+        loggedIn,
+        initializing,
+        error
+    };
 };
 
 Main.propTypes = {
-  actions: PropTypes.object.isRequired,
+    actions: PropTypes.object.isRequired,
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {
-    onAppInit: () => dispatch(actions.initApp()),
-    onLogout: () => dispatch(authActions.logout()),
-    actions: bindActionCreators({showLoading, hideLoading}, dispatch),
-  };
+    return {
+        onAppInit: () => dispatch(actions.initApp()),
+        onLogout: () => dispatch(authActions.logout()),
+        actions: bindActionCreators({showLoading, hideLoading}, dispatch),
+    };
 };
 
 export default withRouter(
-  connect(mapStateToProps, mapDispatchToProps)(Main)
+    connect(mapStateToProps, mapDispatchToProps)(Main)
 );
